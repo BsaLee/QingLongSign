508b35f2cc55e4175a048806c868c4517b3fd268
luqiao <50013416+BsaLee@users.noreply.github.com>
2025-03-01 18:03:52 +0800

    serv00保活

--- serv00.py
+++ serv00.py
@@ -1,8 +1,14 @@
+/*
+    name: "serv00保活"
+    cron: 0 0,8 * * *
+    更新时间:2025-03-01
+    # 环境变量中设置SSH_INFO = host1 username1 password1@host2 username2 password2
+    # 支持多账号,用@分隔
+*/
 import os
 import subprocess
 
-# 环境变量中设置SSH_INFO = host1 username1 password1@host2 username2 password2
-# 支持多账号,用@分隔
+
 # 获取环境变量中的 SSH 信息
 ssh_info = os.getenv("SSH_INFO")
 
